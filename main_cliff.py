f0bb75da44a262f868bebe9292ae1858fe091d73
mwjung <mwjung10@gmail.com>
2025-05-24 21:08:03 +0200

    added some testing

--- main_cliff.py
+++ main_cliff.py
@@ -1,18 +1,64 @@
 import gymnasium as gym
 from q_algorithm import QLearningAgent
+import matplotlib.pyplot as plt
+
+import pandas as pd
+from tabulate import tabulate
+
+
+def average_experiments():
+    env = gym.make("CliffWalking-v0", render_mode="ansi")
+    agent = QLearningAgent(env)
+
+    learning_rates = [0.01, 0.1, 0.5, 1.0]
+    episodes_list = [100, 500, 1000, 2000]
+    n_runs = 25
+
+    results = []
+
+    for lr in learning_rates:
+        for episodes in episodes_list:
+            avg_success = 0
+            avg_fail = 0
+            avg_steps = 0
+            avg_last10 = 0
+
+            for _ in range(n_runs):
+                env = gym.make("CliffWalking-v0")
+                agent = QLearningAgent(env,
+                                     learning_rate=lr)
+
+                metrics = agent.train(max_episodes=episodes)
+                avg_success += metrics["success_rate"]
+                avg_fail += metrics["cliff_fail_rate"]
+                avg_steps += metrics["avg_step_count"]
+                avg_last10 += metrics["rewards_last_10"]
+                env.close()
+
+            avg_success /= n_runs
+            avg_fail /= n_runs
+            avg_steps /= n_runs
+            avg_last10 /= n_runs
+
+            results.append({
+                "Learning rate": lr,
+                "Episodes": episodes,
+                "Success rate": f"{avg_success:.1%}",
+                "Cliff fail rate": f"{avg_fail:.1%}",
+                "Avg steps": f"{avg_steps:.1f}",
+                "Avg last 10 rewards ": f"{avg_last10:.1f}"
+            })
+
+    df = pd.DataFrame(results)
+    df = df.sort_values(by=["Learning rate", "Episodes"])
+    print(tabulate(df, headers='keys', tablefmt='grid', showindex=False))
+
+    return df
 
 
 if __name__ == "__main__":
     env = gym.make("CliffWalking-v0", render_mode="ansi")
     observation, info = env.reset()
 
-    print(env.render())
+    average_experiments()
 
-    print("Action Space {}".format(env.action_space))
-    print("State Space {}".format(env.observation_space))
-
-    agent = QLearningAgent(
-        env
-    )
-
-    agent.train(max_episodes=500)
--- q_algorithm.py
+++ q_algorithm.py
@@ -1,16 +1,26 @@
 import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    handlers=[logging.StreamHandler()])
+
+logging.disable(logging.INFO)
 
 
 class QLearningAgent:
-    def __init__(self, env, learning_rate=0.1, discount_factor=0.99, initial_exploration_prob=1.0, exploration_decay=0.99,
-                 min_exploration_prob=0.01):
+    def __init__(self, env, learning_rate=0.1, discount_factor=0.99, exploration_prob=0.1):
         self.env = env
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
-        self.exploration_prob = initial_exploration_prob
-        self.exploration_decay = exploration_decay
-        self.min_exploration_prob = min_exploration_prob
+        self.exploration_prob = exploration_prob
         self.q_table = np.zeros((env.observation_space.n, env.action_space.n))
+
+        # for checking the accuracy
+        self.success_rate = 0  # meaure how much the agent is successful
+        self.rewards_last_10 = []  # store the last 10 rewards to check the performance
+        self.cliff_fail_rate = 0  # measure how much the agent fails to reach the goal
+        self.avg_step_count = 0  # average number of steps to reach the goal
 
     def _choose_action(self, state):
         if np.random.rand() < self.exploration_prob:
@@ -19,10 +29,13 @@ class QLearningAgent:
             return np.argmax(self.q_table[state])
 
     def train(self, max_episodes=100):
+
+        steps_array = []
         for episode in range(max_episodes):
             state, _ = self.env.reset()
             done = False
             total_reward = 0
+            steps = 0
 
             while not done:
                 action = self._choose_action(state)
@@ -35,12 +48,30 @@ class QLearningAgent:
                 td_error = td_target - self.q_table[state, action]
                 self.q_table[state, action] += self.learning_rate * td_error
                 state = next_state
+                steps += 1
+
+            steps_array.append(steps)
 
             if episode % 100 == 0:
-                print(f"Episode: {episode}, Reward: {total_reward}, Exploration: {self.exploration_prob:.3f}")
+                logging.info(f"Episode: {episode}, Reward: {total_reward}, Exploration: {self.exploration_prob:.3f}")
 
-            self.exploration_prob = max(
-                self.min_exploration_prob,
-                self.exploration_prob * self.exploration_decay
-            )
+
+            # Update success rate, cliff fail rate, and average step count
+            if total_reward >= -100:
+                self.success_rate += 1
+                if ((max_episodes - episode) < 10):
+                    self.rewards_last_10.append(total_reward)
+            else:
+                self.cliff_fail_rate += 1
+        # Calculate success rate, cliff fail rate, and average step count
+        self.avg_step_count = np.mean(steps_array)
+        self.success_rate /= max_episodes
+        self.cliff_fail_rate /= max_episodes
+
+        return {
+            "success_rate": self.success_rate,
+            "cliff_fail_rate": self.cliff_fail_rate,
+            "avg_step_count": self.avg_step_count,
+            "rewards_last_10": np.mean(self.rewards_last_10) if self.rewards_last_10 else 0
+        }
 
